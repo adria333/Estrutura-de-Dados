7949b19d457b1927c8fe6a6979cdc7c3755edb4f
franjolinha <arsfreitas04@gmail.com>
2026-04-08 23:53:30 -0300

    modificações

--- matriz.c
+++ matriz.c
@@ -1,11 +1,42 @@
+//matriz ->> notas do semestre//
+
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
+#include<locale.h>
+
+double ler_num_virgula(){ //transformando letras para numeros//
+
+    char numeros[6]; 
+    double resultado;
+    
+    scanf(" %[^\n]", numeros);
+
+    for(int i=0; i<strlen(numeros); i++){
+        int atual = numeros[i]+0; //atual recebe numero escrito e reescreve como a representacao da tabela ascii//
+
+        if(atual != 46 && atual != 44){ //verificacao de virgula = 44 e ponto = 46//
+            if(atual > 57 || atual < 48){ //verificando se o valor digitado esta dentro do intervalo de numeros esperado (0-9)==(48-57)//
+                return -1.0;
+            }
+        }else if(atual == 44){
+            numeros[i] = '.'; //transforma virgula em ponto//
+        }
+    }
+
+    resultado = atof(numeros); //string -> double//
+    return resultado;
+
+}
 
 int main(){
 
     char materias[7][24]; //declarando matriz para armazenar o nome de 7 materias com no maximo 24 caracteres//
-    float notas[7][3]; //declarando matriz para armazenar 3 notas por materia (atividade, lista, prova)//
-    float media;
+    double notas[7][3]; //declarando matriz para armazenar 3 notas por materia (atividade, lista, prova)//
+    double nota;
+    double media;
+
+    setlocale(LC_NUMERIC, "pt_BR.UTF-8"); //numeros com virgula//
 
     printf("\n\n=== MATERIAS DO SEMESTRE ===\n");
 
@@ -21,13 +52,43 @@ int main(){
         for(int j = 0; j<3; j++){ //lendo as notas//
             if(j == 0){
                 printf("\nNota da Atividade: ");
-                scanf("%f", &notas[i][j]);
+                nota = ler_num_virgula();
+
+                while(nota == -1){
+                    printf("\nErro! Digite Novamente!");
+
+                    printf("\nNota da Atividade: ");
+                    nota = ler_num_virgula();
+                }
+
+                notas[i][j] = nota; 
+
             }else if(j == 1){
                 printf("Nota da Lista: ");
-                scanf("%f", &notas[i][j]);
+                nota = ler_num_virgula();
+
+                while(nota == -1){
+                    printf("\nErro! Digite Novamente!");
+
+                    printf("\nNota da Lista: ");
+                    nota = ler_num_virgula();
+                }
+
+                notas[i][j] = nota; 
+                
             }else{
                 printf("Nota da Prova: ");
-                scanf("%f", &notas[i][j]);
+                nota = ler_num_virgula();
+
+                while(nota == -1){
+                    printf("\nErro! Digite Novamente!");
+
+                    printf("\nNota da Prova: ");
+                    nota = ler_num_virgula();
+                }
+
+                notas[i][j] = nota; 
+                
             }
 
         }
--- vetor.c
+++ vetor.c
@@ -1,16 +1,43 @@
-//vetor controle de gastos semanal//
+//vetor ->> controle de gastos semanal//
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<locale.h>
 
 void dias_da_semana(int i);
 
+double ler_num_virgula(){ //transformando letras para numeros//
+
+    char numeros[6]; 
+    double resultado;
+    
+    scanf(" %[^\n]", numeros);
+
+    for(int i=0; i<strlen(numeros); i++){
+        int atual = numeros[i]+0; //atual recebe numero escrito e reescreve como a representacao da tabela ascii//
+
+        if(atual != 46 && atual != 44){ //verificacao de virgula = 44 e ponto = 46//
+            if(atual > 57 || atual < 48){ //verificando se o valor digitado esta dentro do intervalo de numeros esperado (0-9)==(48-57)//
+                return -1.0;
+            }
+        }else if(atual == 44){
+            numeros[i] = '.'; //transforma virgula em ponto//
+        }
+    }
+
+    resultado = atof(numeros); //string -> double//
+    return resultado;
+
+}
+
 int main(){
 
-    float total_diario[7], gasto_faculdade[7],gasto_extra[7], gasto_trabalho[7];
-    float gasto_semanal = 0.0;
-    float maior_gasto = 0.0;
-    int op_valida = 0;
-    int i, semana;
+    double total_diario[7], gasto_faculdade[7],gasto_extra[7], gasto_trabalho[7];
+    double gasto_semanal = 0.0, op_valida = 0.0;
+    double semana;
+
+    setlocale(LC_NUMERIC, "pt_BR.UTF-8"); 
 
     printf("\n===========================\n");
     printf("CONTROLE DE GASTOS SEMANAIS\n");
@@ -20,7 +47,15 @@ int main(){
     while(op_valida==0){
 
         printf("\nSelecione a Semana do Mes:\n\n1 - Primeira Semana\n2 - Segunda Semana\n3 - Terceira Semana\n4 - Quarta Semana\nDigite: ");
-        scanf("%d", &semana);
+
+        semana = ler_num_virgula();
+
+        while(semana == -1){
+            printf("\nErro! Digite Novamente!");
+
+            printf("\nSelecione a Semana do Mes:\n\n1 - Primeira Semana\n2 - Segunda Semana\n3 - Terceira Semana\n4 - Quarta Semana\nDigite: ");
+                semana = ler_num_virgula();
+        }
 
         if(semana == 1){
         printf("\nSEMANA 01:\n");
@@ -40,17 +75,41 @@ int main(){
     }
 
     //gastos diarios//
-    for(i = 0; i<7; i++){
+    for(int i = 0; i<7; i++){
         dias_da_semana(i);
 
         printf("\nGastos Faculdade: R$ ");
-        scanf("%f", &gasto_faculdade[i]);
+
+        gasto_faculdade[i] = ler_num_virgula();
+
+        while(gasto_faculdade[i] == -1){
+            printf("\nErro! Digite Novamente!");
+
+            printf("\nGastos Faculdade: R$ ");
+                gasto_faculdade[i] = ler_num_virgula();
+        }
 
         printf("Gastos Trabalho: R$ ");
-        scanf("%f", &gasto_trabalho[i]);
+
+        gasto_trabalho[i] = ler_num_virgula();
+
+        while(gasto_trabalho[i] == -1){
+            printf("\nErro! Digite Novamente!");
+
+            printf("\nGastos Trabalho: R$ ");
+                gasto_trabalho[i] = ler_num_virgula();
+        }
 
         printf("Gastos Extras: R$ ");
-        scanf("%f", &gasto_extra[i]);
+
+        gasto_extra[i] = ler_num_virgula();
+
+        while(gasto_extra[i] == -1){
+            printf("\nErro! Digite Novamente!");
+
+            printf("\nGastos Extra: R$ ");
+                gasto_extra[i] = ler_num_virgula();
+        }
 
         total_diario[i] = gasto_extra[i] + gasto_trabalho[i] + gasto_faculdade[i];
 
@@ -85,3 +144,4 @@ void dias_da_semana(int i){
     }
 
 }
+
